56e5a8b53437edb6fa0fac1a03837cced368eafa
husless <2921692+husless@users.noreply.github.com>
2026-06-03 21:57:33 +0800

    Update solution for matrix-addition

--- study-plans/cuda-basics/matrix-addition/matrix-addition.cu
+++ study-plans/cuda-basics/matrix-addition/matrix-addition.cu
@@ -1,17 +1,19 @@
 #include <cuda_runtime.h>
 
-__global__ void matrix_add_kernel(const float* A, const float* B, float* C, int M, int N) {
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    if (i<M && j<N) {
-        int idx = j + i*N;
-        C[idx] = A[idx] + B[idx];
+__global__ void matrix_add_kernel(const float* __restrict__ A, const float*  __restrict__ B, float* __restrict__ C, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = idx; i < N; i += stride) {
+        C[i] = A[i]+B[i];
     }
 }
 
 extern "C" void solve(const float* A, const float* B, float* C, int M, int N) {
-    dim3 threads(16, 16);
-    dim3 blocks((N + 15) / 16, (M + 15) / 16);
-    matrix_add_kernel<<<blocks, threads>>>(A, B, C, M, N);
+    int threads = 256;
+    int total = N*M;
+
+    int blocks = (total + threads - 1) / threads;
+    matrix_add_kernel<<<blocks, threads>>>(A, B, C, total);
     cudaDeviceSynchronize();
 }
